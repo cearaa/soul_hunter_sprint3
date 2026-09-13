f628a8b74b5ed946304f51a9299e236621d3e981
giovannidevazevedo <giovanniazevedo065@gmail.com>
2026-09-13 18:13:14 -0300

    feat: criando pagina para demostração da beta do game

---
+++ src/pages/SoulHunter/SoulHunter.tsx
@@ -0,0 +1,12 @@
+
+
+export default function SoulHunter() {
+    return (
+        <div className="mb-8 text-center flex flex-col ">
+            <h1 className="justify-center text-3xl font-bold text-white sm:text-5xl">Vamos jogar</h1>
+            <p className="">escaneie o qr code cadastre-se e se divirta com a soul hunter em sua versão BETA </p>
+            
+        </div>
+        
+    )
+}
