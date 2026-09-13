f907954c7196996f4c7d17c85c57cdba96af4234
giovannidevazevedo <giovanniazevedo065@gmail.com>
2026-09-13 18:14:59 -0300

    fix: corrigido o alinhamento do texto e a exibição da imagem do qr code na página SoulHunter

--- src/pages/SoulHunter/SoulHunter.tsx
+++ src/pages/SoulHunter/SoulHunter.tsx
@@ -4,8 +4,9 @@ export default function SoulHunter() {
     return (
         <div className="mb-8 text-center flex flex-col ">
             <h1 className="justify-center text-3xl font-bold text-white sm:text-5xl">Vamos jogar</h1>
-            <p className="">escaneie o qr code cadastre-se e se divirta com a soul hunter em sua versão BETA </p>
+             <p className="">escaneie o qr code cadastre-se e se divirta com a soul hunter em sua versão BETA </p>
             
+            <img className="w-45.5" src="https://i.ibb.co/Z6G8nb18/QRCode-F-cil.png" alt="qr code para jogar" />
         </div>
         
     )
